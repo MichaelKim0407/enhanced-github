2006b37162e570d6a0d2d389653c260de2e0bfa4
Michael Kim <mkim0407@gmail.com>
2018-11-10 13:55:36 -0800

    [FEATURE] force reload page when url changes
    GitHub doesn't always reload the page when navigating though folders,
    resulting in content scripts not loading correctly.

---
+++ src/github/refresh.js
@@ -0,0 +1,19 @@
+(function ($, util) {
+    const option = 'gh-refresh';
+
+    var url = null;
+
+    function refresh() {
+        if (url !== null && url !== window.location.href) {
+            console.log('Refreshing page...');
+            window.location.reload(true);
+        }
+        url = window.location.href;
+    }
+
+    $(function () {
+        util.withOption(option, function () {
+            setInterval(refresh, 100);
+        })
+    })
+})(window.jQuery, window.enhancedGithubUtil);
